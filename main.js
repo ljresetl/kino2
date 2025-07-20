170f378b1d59f9bbb67a2cc51f92e2eefb886e83
jres <ljresetl@gmail.com>
2025-07-20 17:41:22 +0200

    up

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ async function loadPartial(id, url) {
   }
 }
 
-loadPartial('header', './partials/header.html');
-loadPartial('search-bar', './partials/search-bar-nav.html');
-loadPartial('filters', './partials/filters.html');
-loadPartial('movies-list', './partials/movies-list.html');
-loadPartial('movies', './partials/movies.html');
-loadPartial('block-new', './partials/block-new.html');
-loadPartial('opis-kino', './partials/opis-kino-ua.html');
-loadPartial('footer', './partials/footer.html');
+loadPartial('header', './header.html');
+loadPartial('search-bar', './search-bar-nav.html');
+loadPartial('filters', './filters.html');
+loadPartial('movies-list', './movies-list.html');
+loadPartial('movies', './movies.html');
+loadPartial('block-new', './block-new.html');
+loadPartial('opis-kino', './opis-kino-ua.html');
+loadPartial('footer', './footer.html');
